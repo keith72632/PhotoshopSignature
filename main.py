b38228b989dff8693f7e0399d144769448bc4baa
keith72632 <keithhorton30@outlook.com>
2021-10-07 20:00:23 -0500

    backup

--- main.py
+++ main.py
@@ -12,12 +12,19 @@ def main():
 	bmr = Document(bmr_file_in, bmr_file_out)
 	bmr.signDocs(bmr_x_pos, bmr_y_pos)
 
-	west_front_y = 930
-	west_front_x = 1015
+	swor_front_y = 930
+	swor_front_x = 1015
 	west_front_file_in = "files/west_swor_front.jpg"
 	west_front_file_out = "finals/west_swor_frontFinale.jpg"
 	west_front = Document(west_front_file_in, west_front_file_out)
-	west_front.signDocs(west_front_x, west_front_y)
+	west_front.signDocs(swor_front_x, swor_front_y)
+
+	swor_back_y = 938
+	swor_back_x = 1000
+	west_back_file_in = "files/west_swor_back.jpg"
+	west_back_file_out = "finals/west_swor_backFinale.jpg"
+	west_back = Document(west_back_file_in, west_back_file_out)
+	west_back.signDocs(swor_back_x, swor_back_y)
 
 
 if __name__ == '__main__':
